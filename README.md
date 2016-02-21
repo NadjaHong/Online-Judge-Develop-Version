b09ed85d5039dc5aff6926f8f156e11a061f69a2
NadjaHong <605490582@qq.com>
2016-02-21 22:17:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+#Online Judge Develop Version
+
+## 项目介绍
+
+* 后端：Python + Django
+
+* 判题服务器使用 Docker 创建
+
+## 注
+
+基于青大OJ项目的二次开发项目，遵循作者要求遵循的 SATA 协议，已 Star 并感谢 Orz
---
+++ submission/serializers.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+from rest_framework import serializers
+
+from account.models import User
+from .models import Submission
+
+
+class CreateSubmissionSerializer(serializers.Serializer):
+    problem_id = serializers.IntegerField()
+    language = serializers.IntegerField()
+    code = serializers.CharField(max_length=20000)
+
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField("_get_submission_user")
+
+    class Meta:
+        model = Submission
+        fields = ["id", "result", "create_time", "language", "user"]
+
+    def _get_submission_user(self, obj):
+        return User.objects.get(id=obj.user_id).username
+
+
+class SubmissionhareSerializer(serializers.Serializer):
+    submission_id = serializers.CharField(max_length=40)
+
+
+class SubmissionRejudgeSerializer(serializers.Serializer):
+    submission_id  = serializers.CharField(max_length=40)
+
+
+class CreateContestSubmissionSerializer(serializers.Serializer):
+    contest_id = serializers.IntegerField()
+    problem_id = serializers.IntegerField()
+    language = serializers.IntegerField()
+    code = serializers.CharField(max_length=20000)
+
+
